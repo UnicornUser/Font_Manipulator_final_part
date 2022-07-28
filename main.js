6994f5946030335102f0d5d5f2ad0fa4ec929d96
UnicornUser <103507859+UnicornUser@users.noreply.github.com>
2022-07-28 22:05:19 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,37 @@
+function setup(){
+    video = createCapture(VIDEO);
+    video.size(400,400);
+    video.position(10,50);
+
+    canvas = createCanvas(800,400);
+    canvas.position(430,130);
+
+    poseNet = ml5.poseNet(video,modelDone);
+    poseNet.on('pose',gotposes);
+}
+
+function draw(){
+    background("#5196e3");
+    fill('#F90093');
+    textSize(difference);
+    text(Peter, 50, 400);
+}
+
+function modelDone(){
+    console.log("PoseNet Is Initialized And Loaded");
+}
+
+function gotposes(results,error){
+    if(error){
+        console.error(error);
+    }
+    if(results.length > 0){
+        console.log(results);
+
+        leftWristX = results[0].pose.leftWrist.x;
+        rightWristX = results[0].pose.rightWrist.x;
+        difference = floor(leftWristX - rightWristX);
+
+        
+    }
+}
